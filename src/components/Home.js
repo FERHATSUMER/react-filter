2bdfd5a3a58a3cd4786cdb696150170e314caef3
FERHATSUMER <sumerf39@gmail.com>
2023-03-01 19:13:41 +0300

    react-random-numbers

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import "./home.css";
+import Data from "./Data.json";
+import { useState } from "react";
+
+function Home() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="concainer">
+      <h1> Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          type="text"
+          placeholder="Konsey Üyesi Giriniz..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
